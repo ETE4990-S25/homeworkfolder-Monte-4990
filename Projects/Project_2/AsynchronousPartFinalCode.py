9dd135210da21b4ded92d70a1588f60bbdec23df
Anthony Montes <acmontes@cpp.edu>
2025-04-04 17:07:31 -0700

    added more comments and equations

--- Projects/Project_2/AsynchronousPartFinalCode.py
+++ Projects/Project_2/AsynchronousPartFinalCode.py
@@ -8,13 +8,15 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
-
+# created fibonacci equation using logic
 def fibonacci(n):
     a, b = 0, 1
     for _ in range(n):
         a, b = b, a + b
     return a
 
+
+# factorial equation
 def factorial(n):
     result = 1
     for i in range(2, n + 1):
