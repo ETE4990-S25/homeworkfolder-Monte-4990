29f1269f2f8248023e4b8fe8827f0d27b0290982
Anthony Montes <acmontes@cpp.edu>
2025-04-18 19:57:19 -0700

    changes for whatever, lecture, lab

---
+++ Labs/Lab9/Lab7PythonRUn.py
@@ -0,0 +1,27 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
+
+# Create the logger
+debuglog = logging.getLogger("my_logger")
+
+debuglog.log(logging.CRITICAL, "OMG THE WORLD IS ENDING!!!!")
+
+debuglog.critical("there is no more disk space")
+debuglog.error("file not found creating new file")
+debuglog.warning("disk space is low")
+debuglog.info("user logged in with correct password")
+
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s: %(name)s',force=True)
+
+logging.getLogger("my_app")
+logging.getLogger("my_app.utils")
+logging.getLogger("my_app.utils.db")
+
+formatter = logging.Formatter(
+    fmt=(
+        "%(asctime)s | %(levelname)s | "
+        "%(message)s"
+    )
+)
