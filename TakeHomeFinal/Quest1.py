7bcfae8c7584bccfd38ba74331270cfaad5b95e7
Anthony Montes <acmontes@cpp.edu>
2025-05-17 22:08:49 -0700

    added different exchange rates

--- TakeHomeFinal/Quest1.py
+++ TakeHomeFinal/Quest1.py
@@ -1,5 +1,24 @@
+import os
+import requests
 import xmltodict
 import json
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime, timedelta
+import random
+import logging
+
+logging.basicConfig(filename='logs/quest.log',
+                    level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+BASE_URL = "https://www.floatrates.com/historical-exchange-rates.html"
+OUTPUT_DIR = "data"
+MAX_THREADS = 10
+RATES = ["EUR", "GBP", "USD", "DZD", "AUD", "BWP", "BND", "CAD", "CLP", "CNY", "COP", "CZK",
+         "DKK", "HUF", "ISK", "INR", "IDR", "ILS", "KZT", "KRW", "KWD", "LYD", "MYR", "MUR",
+         "NPR", "NZD", "NOK", "OMR", "PKR", "PLN", "QAR", "RUB", "SAR", "SGD", "ZAR", "LKR",
+         "SEK", "CHF", "THB", "TTD"]
+BASES = [r for r in RATES if r not in ["USD", "EUR", "GBP"]]
 
 def parse_xml_to_json(xml_text: str) -> dict:
     """
